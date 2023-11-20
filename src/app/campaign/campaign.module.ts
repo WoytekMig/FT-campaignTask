4cfbd3fd387246b74dcfe25d61086a0249121825
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-11-20 12:37:47 +0100

    added 'navigation'

--- src/app/campaign/campaign.module.ts
+++ src/app/campaign/campaign.module.ts
@@ -4,9 +4,16 @@ import { CampaignFormComponent } from '../campaign-form/campaign-form.component'
 import { CampaignListComponent } from '../campaign-list/campaign-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HomeModule } from '../home/home.module';
 
 @NgModule({
   declarations: [CampaignFormComponent, CampaignListComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HomeModule,
+  ],
 })
 export class CampaignModule {}
