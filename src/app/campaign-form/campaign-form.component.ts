6207c1c4d690778679788fa4d8e8127cb13823cc
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-11-20 02:32:07 +0100

    added validators

--- src/app/campaign-form/campaign-form.component.ts
+++ src/app/campaign-form/campaign-form.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validator } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-campaign-form',
   templateUrl: './campaign-form.component.html',
   styleUrls: ['./campaign-form.component.css'],
 })
-export class CampaignFormComponent {
+export class CampaignFormComponent implements OnInit {
   campaignForm: FormGroup = new FormGroup({});
 
-  onSubmit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.campaignForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      tags: ['', Validators.required],
+      bidAmount: [
+        '',
+        Validators.required,
+      ] /* ['', [Validators.required, Validators.min(1000)]], */,
+      campaignFund: ['', Validators.required],
+      status: ['', Validators.required],
+      /* ['', [Validators.required, Validators.requiredTrue]],  */
+      town: ['Cracow'],
+      radius: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.campaignForm.valid) {
+      console.log('VALID');
+    }
+  }
 }
