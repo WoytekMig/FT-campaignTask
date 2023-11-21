c303ab951f46d21c83dbe9072ec1f281be0a71c1
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-11-21 12:35:55 +0100

    added pre-populated keywords

--- src/app/campaign-form/campaign-form.component.ts
+++ src/app/campaign-form/campaign-form.component.ts
@@ -14,6 +14,7 @@ export class CampaignFormComponent implements OnInit {
   campaignForm: FormGroup = new FormGroup({});
   townOptions: string[] = ['Cracow', 'Warsaw', 'Poznan', 'Gdansk', 'Lodz'];
   accountStatus: number = this.accountStatusService.getAccountStatus();
+  keywords: string[] = ['TV', 'free-time', 'sport', 'food', 'drink', 'wear'];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -21,23 +22,26 @@ export class CampaignFormComponent implements OnInit {
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private accountStatusService: AccountStatusService
-  ) {}
-
-  ngOnInit(): void {
-    const accountStatus = this.accountStatusService.getAccountStatus();
+  ) {
     this.campaignForm = this.formBuilder.group({
       name: ['', Validators.required],
-      keywords: ['', Validators.required],
+      selectedKeywords: [[]],
       bidAmount: ['', [Validators.required, Validators.min(1500)]],
       campaignFund: [
         '',
-        [Validators.required, Validators.min(0), Validators.max(accountStatus)],
+        [
+          Validators.required,
+          Validators.min(0),
+          Validators.max(this.accountStatus),
+        ],
       ],
       status: ['', Validators.required],
       town: [''],
       radius: ['', [Validators.required, Validators.min(0)]],
     });
+  }
 
+  ngOnInit(): void {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
 
     if (id) {
@@ -58,6 +62,7 @@ export class CampaignFormComponent implements OnInit {
   onSubmit() {
     if (this.campaignForm.valid) {
       let campaign: Campaign = this.campaignForm.value;
+      campaign.keywords = this.campaignForm.value.selectedKeywords;
 
       let id = this.activatedRoute.snapshot.paramMap.get('id');
       if (id) {
